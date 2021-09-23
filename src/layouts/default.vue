465761d39bdd7121a5b1fd51fb971beddf81c771
Daria Kriukova <m.decarabia@gmail.com>
2021-09-24 02:40:07 +0300

    feat: add layout w/ header

---
+++ src/layouts/default.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-header :text="headerTitle">
+    <button
+      v-if="routeName === 'Result'"
+      @click="$router.push('/')"
+      style="font-size: 1rem; height: 45px"
+    >
+      Вернуться на главную
+    </button>
+  </v-header>
+  <slot />
+</template>
+<script>
+import VHeader from "../components/VHeader";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "LayoutDefault",
+  components: { VHeader },
+  data() {
+    return {
+      pathTitles: {
+        Home: "Добро пожаловать",
+        Result: "Результат",
+      },
+    };
+  },
+  computed: {
+    headerTitle() {
+      switch (this.routeName) {
+        case "Home":
+          return "Добро пожаловать";
+        case "Result":
+          return "Результат";
+        default:
+          return "Добро пожаловать";
+      }
+    },
+    routeName() {
+      return this.$route.name;
+    },
+  },
+});
+</script>
