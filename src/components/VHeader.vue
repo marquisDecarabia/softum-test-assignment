c560d30e18a28502c5dbf56b7f4c6328b6b20555
Daria Kriukova <m.decarabia@gmail.com>
2021-09-24 02:00:03 +0300

    feat: add header component

---
+++ src/components/VHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header">
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "VHeader",
+  props: {
+    text: String,
+  },
+});
+</script>
+
+<style scoped>
+.header {
+  border-bottom: 1px solid black;
+  margin-bottom: 5rem;
+  font-size: 1.33rem;
+}
+</style>
